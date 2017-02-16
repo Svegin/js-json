47b1eac0a1a64db7613dbe45fa78c98f159e1afb
Svegin <svegin@Svegins-Mac-mini.local>
2017-02-15 21:09:46 -0600

    load json use jquery

--- myscript.js
+++ myscript.js
@@ -1,16 +1,7 @@
-var request;
-if (window.XMLHttpRequest) {
-	request=new XMLHttpRequest();
-} else {
-	request=new ActiveXObject("Microsoft.XMLHTTP");
-}
+$(document).ready(function() {
 
-request.open('GET', 'data.json');
-request.onreadystatechange = function() {
-	if ((request.status === 200) &&
-		(request.readyState === 4)) {
+$.getJSON('data.json', function(info) {
 
-			var info = JSON.parse(request.responseText);
 			var output='';
 			for (var i = 0; i <= info.links.length-1; i++) {
 				for (key in info.links[i]) {
@@ -25,8 +16,7 @@ request.onreadystatechange = function() {
 			
 			var update = document.getElementById('links');
 			update.innerHTML = output;
-			
-			
-	} //ready
-} //event
-request.send();
+});	// getJSON
+
+
+}); //ready
